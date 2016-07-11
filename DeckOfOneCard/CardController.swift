5c39b6606a4cde4bf410dc6c4e32bdf02a5e2ef6
pattyhackins <patrickridd@yahoo.com>
2016-07-11 11:28:23 -0600

    setup network controller class and methods

---
+++ DeckOfOneCard/CardController.swift
@@ -0,0 +1,20 @@
+//
+//  CardController.swift
+//  DeckOfOneCard
+//
+//  Created by Patrick Ridd on 7/11/16.
+//  Copyright © 2016 PatrickRidd. All rights reserved.
+//
+
+import Foundation
+
+class CardController {
+    
+    static let baseURL = NSURL(string: "http://deckofcardsapi.com/api/deck/new/draw/")
+    
+    static func drawCards(numberOfCards: Int, completion: ((card: [Card]) -> Void)) {
+        
+    }
+    
+    
+}
